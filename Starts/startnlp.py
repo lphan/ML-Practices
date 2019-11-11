3c5b593842f20fe9903e09d9449f57c4574d853d
Long Phan <long.phan@uni-dortmund.de>
2019-11-11 22:55:05 +0100

    Update README.md and add TODO new module startnlp

---
+++ Starts/startnlp.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2018
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+__author__ = 'Long Phan'
+
+
+class StartNLP(object):
+    """
+        Description: StartNLP - Start package Natural Language Processing
+            - Implementation of Recursive Neural Network (Tree NN)
+            - Implementation of Recursive Neural Tensor Network
+            - Implementation of Recurrent Neural Network            
+
+        References:
+            Theano
+            Tensorflow
+            
+        Start:
+          jupyter notebook
+          -> from startnlp import *
+          -> info_startnlp
+    """
+    pass
+    
