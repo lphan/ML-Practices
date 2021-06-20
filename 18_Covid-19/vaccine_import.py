b5f6556ab0da9a5db4d7cfb091006349d989ac45
lphan <long.phan@uni-dortmund.de>
2021-06-20 14:48:34 +0200

    Update result 19.06.2021 and update time series vaccines

--- 18_Covid-19/vaccine_import.py
+++ 18_Covid-19/vaccine_import.py
@@ -18,8 +18,18 @@ Start._arguments()
 raw_data_us = './data/Vaccine/COVID-19/data_tables/vaccine_data/us_data/time_series/vaccine_data_us_timeline.csv'
 raw_data_global = './data/Vaccine/COVID-19/data_tables/vaccine_data/global_data/vaccine_data_global.csv'
 
+ts_vaccine_global = './data/Vaccine/COVID-19/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_global.csv'
+ts_vaccine_doses_global = './data/Vaccine/COVID-19/data_tables/vaccine_data/global_data/time_series_covid19_vaccine_doses_admin_global.csv'
+
 print("Start importing single data vaccine in US", raw_data_us)
 total_confirmed_vaccine_us = Start.import_data(raw_data_us)
 
 print("Start importing single data vaccine in the World", raw_data_global)
 total_confirmed_vaccine_global = Start.import_data(raw_data_global)
+
+print("Start importing time series data vaccine in the World", ts_vaccine_global)
+time_series_vaccine_global = Start.import_data(ts_vaccine_global)
+
+# print("Start importing time series data vaccine doses admin in the World", ts_vaccine_doses_global)
+# time_series_admin_vaccine_global = Start.import_data(ts_vaccine_doses_global)
+
